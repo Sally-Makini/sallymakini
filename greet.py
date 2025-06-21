0205e92b03ff351db36ad63c61098f61155cc48a
Sally-Makini <sallymakini@gmail.com>
2025-06-21 12:10:53 +0300

    additional

--- greet.py
+++ greet.py
@@ -1,2 +1,7 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! Have a great day!")
+
+color = input("What's your favorite color? ")
+print(f"Wow! {color} is a beautiful color, {name}!")
+
+print("Thanks for chatting with me 😊")
